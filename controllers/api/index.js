8b590fb9d0ee7d7a3ee6c3ce2a378e60b2992bb6
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-03 15:05:54 -0700

    required different routes in index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const familyRoutes = require('./family-routes');
+const fileRoutes = require('./file-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/users', userRoutes);
+router.use('/familys', familyRoutes);
+router.use('/files', fileRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
