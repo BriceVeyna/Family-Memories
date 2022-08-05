8e46a09223dc87d78b3f25f4c97edee870fdca8a
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-05 15:35:28 -0700

    added js function for creating a new family

--- public/js/script.js
+++ public/js/script.js
@@ -27,3 +27,28 @@ const newFile = async (event) => {
 // button to submit file
 document.querySelector(".file-btn").addEventListener("submit", newFile);
 
+//function to create a new family
+const newFamily = async (event) => {
+    event.preventDefault();
+  
+    const familyName = document.querySelector("#family-name").value.trim();
+  
+    if (familyName) {
+      const response = await fetch("/api/fileFamily", {
+        //need different route?
+        method: "POST",
+        body: JSON.stringify({ familyName }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        document.location.replace("/api/family");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  // button to submit family
+  document.querySelector(".family-btn").addEventListener("submit", newFamily);
+  
