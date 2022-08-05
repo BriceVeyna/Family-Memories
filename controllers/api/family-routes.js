7aded5515006a516e2db11f8c7fa8d32f7b271b8
Phillip <phillip.besse@gmail.com>
2022-08-04 20:49:45 -0400

    deployment bug

--- controllers/api/family-routes.js
+++ controllers/api/family-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Family } = require("../../models");
+const withAuth = require('../../utils/Auth')
 
 // get all families
 router.get("/", async (req, res) => {
