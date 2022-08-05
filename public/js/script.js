0dc0cb17b36c87fe473cc7147cb236c581b10ce0
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-05 15:35:37 -0700

    added js function for creating a new comment

--- public/js/script.js
+++ public/js/script.js
@@ -52,3 +52,27 @@ const newFamily = async (event) => {
   // button to submit family
   document.querySelector(".family-btn").addEventListener("submit", newFamily);
   
+  //function to create a new comment
+  const newComment = async (event) => {
+    event.preventDefault();
+  
+    const commentText = document.querySelector("#comment-text").value.trim();
+  
+    if (commentText) {
+      const response = await fetch("/api/file/comment", {
+        //need different route?
+        method: "POST",
+        body: JSON.stringify({ commentText }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        document.location.replace("/api/file");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  // button to submit comment
+  document.querySelector(".comment-btn").addEventListener("submit", newComment);
