6298398d3a323a90dc8311e66cffb3e2bfa95352
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-07 14:55:02 -0700

    added function in comment.js to access text input

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,24 +1,27 @@
-// //function to create a new comment
-// const newComment = async (event) => {
-//     event.preventDefault();
-  
-//     const commentText = document.querySelector("#comment-text").value.trim();
-  
-//     if (commentText) {
-//       const response = await fetch("/api/file/comment", {
-//         //need different route?
-//         method: "POST",
-//         body: JSON.stringify({ commentText }),
-//         headers: { "Content-Type": "application/json" },
-//       });
-  
-//       if (response.ok) {
-//         document.location.replace("/api/file");
-//       } else {
-//         alert(response.statusText);
-//       }
-//     }
-//   };
-  
-//   // button to submit comment
-//   document.querySelector(".comment-btn").addEventListener("submit", newComment);
+//function to create a new comment
+const newComment = async (event) => {
+  event.preventDefault();
+
+  const commentText = document.querySelector("#comment-text").value.trim();
+
+  if (commentText) {
+    const response = await fetch("/file", { // add id after file
+      method: "POST",
+      body: JSON.stringify({ commentText }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      alert("comment working");
+      document.location.replace("/file"); // add id after file
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+// button to submit comment
+const newCommentButton = document.getElementById("comment-button");
+if (newCommentButton) {
+  newCommentButton.addEventListener("click", newComment);
+}
