c7e5cf542e9c5ccaf842f45559f6d5acf9d0c49f
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-06 21:42:43 -0700

    added home button to reroute

--- public/js/file.js
+++ public/js/file.js
@@ -3,12 +3,12 @@ const filePage = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/file/:${id}`, {
+    const response = await fetch(`/api/file/${id}`, {
       method: 'GET',
     });
 
     if (response.ok) {
-      document.location.replace(`/api/file/:${id}`);
+      document.location.replace(`/api/file/${id}`);
     } else {
       alert('Failed to open file.');
     }
@@ -19,34 +19,4 @@ const filePage = async (event) => {
 const openFile = document.getElementById('file-name');
 if(openFile){
   openFile.addEventListener('click', filePage);
-}
-
-// creates a new file
-const newFile = async (event) => {
-  event.preventDefault();
-
-  const fileName = document.querySelector('#inputFileName').value.trim();
-  const fileDescription = document.querySelector('#inputFileDescription').value.trim();
-  const fileUrl = document.querySelector('#inputFileURL').value.trim();
-
-  if (fileName && fileDescription && fileUrl) {
-    const response = await fetch(`/api/file`, {
-      method: 'POST',
-      body: JSON.stringify({ fileName, fileDescription, fileUrl }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to create new file.');
-    }
-  }
-};
-//submit button to create a new file
-const newFileButton = document.getElementById('new-file-button');
-if(newFileButton){
-  newFileButton.addEventListener('click', newFile);
 }
--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,9 @@
-// document.getElementById("home-button").addEventListener("click", homePage)
-
-//   function homePage() {
-//     document.location.replace("/")
-//   }
+function homePage() {
+    document.location.replace("/");
+  }
+  
+  const homeButton = document.getElementById("home-button");
+  if (homeButton) {
+    homeButton.addEventListener("click", homePage);
+  }
+  
