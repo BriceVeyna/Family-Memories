8a56b61d222ded8d288a74bdc86a70ef1bfced59
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-06 23:08:26 -0700

    created get route to add new file - fixed issue

--- public/js/add-file.js
+++ public/js/add-file.js
@@ -9,24 +9,20 @@
 
 
 // reroutes homepage to filepage 
-const newFilePage = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/file/${id}`, {
-      method: 'POST',
+const newFilePage = async () => {
+    const response = await fetch(`/api/file`, {
+      method: 'GET',
     });
 
     if (response.ok) {
-      document.location.replace(`/api/file/${id}`);
+      document.location.replace(`/api/file`);
     } else {
       alert('Failed to create file.');
     }
-  }
 };
 
 // button to route to file page
-const openNewFile = document.getElementById('new-file-button');
-if(openNewFile){
-  openNewFile.addEventListener('click', newFilePage);
+const openFileForm = document.getElementById('new-file-button');
+if(openFileForm){
+  openFileForm.addEventListener('click', newFilePage);
 }
