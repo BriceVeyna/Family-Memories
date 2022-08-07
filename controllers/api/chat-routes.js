28e348968c445d262e26a13f715093de00b7994a
Phillip <phillip.besse@gmail.com>
2022-08-06 21:48:43 -0400

    chat routes

--- controllers/api/chat-routes.js
+++ controllers/api/chat-routes.js
@@ -2,18 +2,26 @@ const router = require('express').Router();
 const { Message } = require('../../models');
 const withAuth = require('../../utils/Auth');
 
-router.get('/', async (req, res) => {
-    try {
-        const messageData = Message.findAll();
-        const messages = messageData.map((message) => message.get({ plain: true }));
+// router.get('/', async (req, res) => {
+//     try {
+//         const messageData = Message.findAll();
+//         const messages = messageData.map((message) => message.get({ plain: true }));
 
-        res.render('chat/messages', {
-            messages,
-            // loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-    res.status(500).json(err);
-}
-});
+//         res.render('chat/messages', {
+//             messages,
+//             // loggedIn: req.session.loggedIn,
+//         });
+//     } catch (err) {
+//     res.status(500).json(err);
+// }
+// });
+
+router.get('/', (req, res) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        res.render('chat', {loggedIn: req.session.loggedIn});
+    }
+})
 
 module.exports = router;
