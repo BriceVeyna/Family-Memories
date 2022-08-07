5912db42eba5e2b35c53432ef55eff40cd33abf2
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-06 21:51:34 -0700

    created fetch route in new file js

--- public/js/new-file.js
+++ public/js/new-file.js
@@ -1,29 +1,32 @@
-// creates a new file
-const newFile = async (event) => {
-    event.preventDefault();
-  
-    const fileName = document.querySelector('#inputFileName').value.trim();
-    const fileDescription = document.querySelector('#inputFileDescription').value.trim();
-    const fileUrl = document.querySelector('#inputFileURL').value.trim();
-  
-    if (fileName && fileDescription && fileUrl) {
-      const response = await fetch(`/api/file`, {
-        method: 'POST',
-        body: JSON.stringify({ fileName, fileDescription, fileUrl }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/api/file');
-      } else {
-        alert('Failed to create new file.');
-      }
+// function newFilePage() {
+//   document.location.replace("/");
+// }
+
+// const addFileButton = document.getElementById("new-file-button");
+// if (addFileButton) {
+//   addFileButton.addEventListener("click", newFilePage);
+// }
+
+
+// reroutes homepage to filepage 
+const newFilePage = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/file/${id}`, {
+      method: 'POST',
+    });
+
+    if (response.ok) {
+      document.location.replace(`/api/file/${id}`);
+    } else {
+      alert('Failed to create file.');
     }
-  };
-  //submit button to create a new file
-  const newFileButton = document.getElementById('new-file-button');
-  if(newFileButton){
-    newFileButton.addEventListener('click', newFile);
   }
+};
+
+// button to route to file page
+const openNewFile = document.getElementById('new-file-button');
+if(openNewFile){
+  openNewFile.addEventListener('click', newFilePage);
+}
