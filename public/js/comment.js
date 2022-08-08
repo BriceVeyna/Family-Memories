d39a8d1728a53713bcbf1f19cd85a86e85d4e4c6
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-07 17:40:45 -0700

    fixed bug with file page rendering

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,19 +3,23 @@ const newComment = async (event) => {
   event.preventDefault();
 
   const commentText = document.querySelector("#comment-text").value.trim();
+  console.log(commentText)
 
-  if (commentText) {
-    const response = await fetch("/file", { // add id after file
-      method: "POST",
-      body: JSON.stringify({ commentText }),
-      headers: { "Content-Type": "application/json" },
-    });
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
 
-    if (response.ok) {
-      alert("comment working");
-      document.location.replace("/file"); // add id after file
-    } else {
-      alert(response.statusText);
+    if (commentText) {
+      const response = await fetch(`/file/${id}`, {
+        method: "POST",
+        body: JSON.stringify({ commentText }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        document.location.replace(`/file/${id}`);
+      } else {
+        alert(response.statusText);
+      }
     }
   }
 };
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
 /////
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
 let onlineUsers = {};
 io.on("connection", (socket) => {
-    require('./config/socket')(io, socket, onlineUsers);
-})
+  require("./config/socket")(io, socket, onlineUsers);
+});
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -38,12 +38,12 @@ app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
