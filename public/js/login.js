e368677ebcc9f7e89d52313fc02bbc7b27e75ad7
Phillip <phillip.besse@gmail.com>
2022-08-07 21:01:53 -0400

    discard

--- public/js/login.js
+++ public/js/login.js
@@ -22,30 +22,4 @@ const loginFormHandler = async (event) => {
 const loginButton = document.getElementById('login-button');
 if(loginButton){
   loginButton.addEventListener('click', loginFormHandler);
-}
-
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector("#inputUsername").value.trim();
-  const password = document.querySelector("#inputPassword").value.trim();
-
-  if (username && password) {
-    const response = await fetch("/api/signup", {
-      method: "POST",
-      body: JSON.stringify({ username, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert(response.statusText);
-    }
-  }
 };
-
-const signupButton = document.getElementById('signup-button');
-if(signupButton){
-  signupButton.addEventListener('click', signupFormHandler);
-}
