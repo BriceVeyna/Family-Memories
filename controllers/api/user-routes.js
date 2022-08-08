0902e0755a6c55baf6e99b80d6e3f146f0df110c
Phillip <phillip.besse@gmail.com>
2022-08-07 22:25:16 -0400

    fix signup

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -29,7 +29,10 @@ router.get('/:id', async (req, res) => {
 //create user
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create({...req.body,});
+    const userData = await User.create({
+      username: req.body.username,
+      password: req.body.password
+    });
 
     req.session.save(() => {
       req.session.user_id = userData.id;
